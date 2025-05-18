4210a44ea323c9d0ff0073813459d395814a8663
Sameer Guruaj Mathad <sameeragmathad@gmail.com>
2025-05-18 16:39:29 -0500

    trying modified code

--- sculpgen/sculpgen/__init__.py
+++ sculpgen/sculpgen/__init__.py
@@ -2,9 +2,10 @@
 Experimental
 """
 
-__all__ = ["Logger", "RYaml", "VGG19"]
+__all__ = ["Logger", "Model", "RYaml", "VGG19"]
 
 
 from .logger import Logger
 from .ryaml import RYaml
 from .vgg_model import VGG19
+from .model import Model
--- sculpgen/sculpgen/vgg_model.py
+++ sculpgen/sculpgen/vgg_model.py
@@ -1,14 +1,14 @@
+from typing import Optional
 import torch
 from torchvision import models
 from torch import nn
 
-torch.manual_seed(123)
-
 
 class VGG19(nn.Module):
 
-    def __init__(self):
+    def __init__(self, random_seed: Optional[int] = 42):
         super().__init__()
+        torch.manual_seed(random_seed)
         self._layers = ["7", "10", "14", "19", "25"]
         self._model = models.vgg19(pretrained=True).features
 
