e51cb4e2f4b9c4b7ecf130e5c5938be0d6abf247
Sameer Guruaj Mathad <sameeragmathad@gmail.com>
2025-05-18 19:27:30 -0500

    not working

--- sculpgen/sculpgen/model.py
+++ sculpgen/sculpgen/model.py
@@ -19,7 +19,7 @@ class Model(RYaml):
 
         random_seed = self._content["train"]["random_seed"]
         torch.manual_seed(random_seed)
-        random.seed(random_seed)
+        # random.seed(random_seed) # You can uncomment if you want similar images.
 
         self._device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -50,28 +50,37 @@ class Model(RYaml):
                 ),  # ImageNet stats.
             ]
         )
+        self._denormalize = transforms.Normalize(
+            (-2.12, -2.04, -1.80), (4.37, 4.46, 4.44)
+        )
 
     def content_loss(self, content: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         """Experimental"""
-        return torch.mean((content - target) ** 2)
+        content_loss = torch.mean((content - target) ** 2)
+        return content_loss
 
     def gram_matrix(self, image: torch.Tensor) -> torch.Tensor:
         """Experimental"""
         channel, width, height = image.shape
         image = image.view(channel, width * height)
-        return image @ image.T
+        gram_mat = (image @ image.T) / (channel * width * height)
+
+        return gram_mat
 
     def style_loss(self, style: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         """Experimental"""
         style_gram = self.gram_matrix(style)
         target_gram = self.gram_matrix(target)
-        return torch.mean((style_gram - target_gram) ** 2)
+        style_loss = torch.mean((style_gram - target_gram) ** 2)
+
+        return style_loss
 
     def train(
         self,
         content_image: torch.Tensor,
         style_image: torch.Tensor,
         target_image: torch.Tensor,
+        *args,
     ):
         """Experimental"""
         optimizer = torch.optim.Adam(
@@ -83,18 +92,21 @@ class Model(RYaml):
         style_image = style_image.to(self._device)
         target_image = target_image.to(self._device)
 
+        target_image.requires_grad = True
+        content_image_num, style_image_num = args
+
         for _ in tqdm(range(epoch), colour="blue"):
-            conent_features = self._model(content_image)
+            content_features = self._model(content_image)
+            print("content", content_features[0].requires_grad)
             style_features = self._model(style_image)
             target_features = self._model(target_image)
-
             optimizer.zero_grad()
 
             style_loss = 0.0
             content_loss = 0.0
 
             for content_feature, style_feature, target_feature in zip(
-                conent_features, style_features, target_features
+                content_features, style_features, target_features
             ):
                 content_loss += self.content_loss(
                     content=content_feature, target=target_feature
@@ -107,10 +119,15 @@ class Model(RYaml):
                 self._content["train"]["alpha"] * content_loss
                 + self._content["train"]["beta"] * style_loss
             )
-
             loss.backward()
+
             optimizer.step()
-        # return target_image
+
+        target_image = self._denormalize(target_image)
+        save_image(
+            target_image,
+            fp=f"{self._save_path}/{self._content['train']['save_prefix']}content_{content_image_num}_style_{style_image_num}{self._content['train']['ext']}",
+        )
 
     def convert_nst(self):
         """Experimental"""
@@ -119,67 +136,22 @@ class Model(RYaml):
                 image = self._transforms(image)
                 for _ in range(self._content["train"]["images_per_content"]):
                     style_image_num = random.randint(a=1, b=self._styles_len)
-                    # style_image_num = 1
+
                     selected_filename = os.path.join(
                         self._content["train"]["abstract_folder"],
                         f"{self._content['train']['abstract_prefix']}{style_image_num}{self._content['train']['ext']}",
                     )
-                    print(selected_filename)
-                    style_image = Image.open(selected_filename)
+
+                    style_image = Image.open(selected_filename).convert("RGB")
 
                     style_image = self._transforms(style_image)
-                    # print(image.shape)
-                    target_image = image.clone()
-                    target_image.requires_grad = True
 
-                    # self.train(
-                    #     content_image=image,
-                    #     style_image=style_image,
-                    #     target_image=target_image,
-                    # )
+                    target_image = image.clone().detach()
 
-                    optimizer = torch.optim.Adam(
-                        [target_image], lr=self._content["train"]["learning_rate"]
-                    )
-                    epoch = self._content["train"]["epochs"]
-
-                    content_image = image.to(self._device)
-                    style_image = style_image.to(self._device)
-                    target_image = target_image.to(self._device)
-
-                    for _ in tqdm(range(epoch), colour="blue"):
-                        content_features = self._model(content_image)
-                        style_features = self._model(style_image)
-                        target_features = self._model(target_image)
-
-                        optimizer.zero_grad()
-
-                        style_loss = 0.0
-                        content_loss = 0.0
-
-                        for content_feature, style_feature, target_feature in zip(
-                            content_features, style_features, target_features
-                        ):
-                            content_loss += self.content_loss(
-                                content=content_feature, target=target_feature
-                            )
-                            style_loss += self.style_loss(
-                                style=style_feature, target=target_feature
-                            )
-
-                        loss: torch.Tensor = (
-                            self._content["train"]["alpha"] * content_loss
-                            + self._content["train"]["beta"] * style_loss
-                        )
-
-                        loss.backward()
-                        optimizer.step()
-
-                    target_image = transforms.Normalize(
-                        (-2.12, -2.04, -1.80), (4.37, 4.46, 4.44)
-                    )(target_image)
-
-                    save_image(
+                    self.train(
+                        image,
+                        style_image,
                         target_image,
-                        fp=f"{self._save_path}/{self._content['train']['save_prefix']}content_{content_image_num}_style_{style_image_num}{self._content['train']['ext']}",
+                        content_image_num,
+                        style_image_num,
                     )
