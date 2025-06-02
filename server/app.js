4e3b8294a87a303263510c76aa8124abd8ddf63a
sameera-g-mathad <sameeragmathad@gmail.com>
2025-06-01 20:06:49 -0500

    server: uncommited changes

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 const chathistRouter = require('./routers/chathistRouter');
@@ -5,64 +6,14 @@ const chathistRouter = require('./routers/chathistRouter');
 // create app.
 const app = express();
 
+app.use(cors());
+
 app.use(morgan('dev'));
+
 // This middleware adds {body} in request
 app.use(express.json());
 
 app.use('/api/v1', chathistRouter);
-
-// app.post('/api/v1/title', async (req, res) => {
-//   try {
-//     const { prompt } = req.body;
-//     const response = await fetch('http://127.0.0.1:8001/api/v1/title', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ prompt }),
-//     });
-//     const data = await response.json();
-//     res.status(200).json(data);
-//   } catch (e) {
-//     res.status(500).json({
-//       status: 'error',
-//       data: { message: 'Internal error!!!' },
-//     });
-//   }
-// });
-
-// app.post('/api/v1/query', async (req, res) => {
-//   try {
-//     const { prompt } = req.body;
-//     const response = await fetch('http://127.0.0.1:8000/api/v1/query', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ prompt }),
-//     });
-//     const reader = await response.body.getReader();
-//     const streamer = new Readable({
-//       async read() {
-//         try {
-//           const { done, value } = await reader.read();
-//           if (!done) {
-//             this.push(Buffer.from(value));
-//           } else this.push(null);
-//         } catch (err) {
-//           this.destroy(err);
-//         }
-//       },
-//     });
-//     streamer.pipe(res);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({
-//       status: 'error',
-//       data: { message: 'Internal error!!!' },
-//     });
-//   }
-// });
 
 app.use('/*all', (req, res) => {
   console.log(req.hostname);
